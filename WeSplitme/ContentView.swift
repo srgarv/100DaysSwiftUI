f7135b5b49702ecc85a8fc57b4c667fcaf2ad56f
Scott Garvey <srgarv@gmail.com>
2020-07-09 15:44:01 -0700

    Initial Commit

---
+++ WeSplitme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplitme
+//
+//  Created by Scott Garvey on 7/9/20.
+//  Copyright © 2020 Scott Garvey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
